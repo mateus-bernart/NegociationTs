f9f066fd3f1d81f5a98ef0315dc06aa2272ebc8b
Mateus Bernart <mateusbernart14@gmail.com>
2025-01-16 08:38:20 -0300

    add nullable parameter

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,12 +1,22 @@
 export abstract class View<T> {
   protected elemento: HTMLElement;
+  private escapar = false;
 
-  constructor(seletor: string) {
+  constructor(seletor: string, escapar?: boolean) {
     this.elemento = document.querySelector(seletor);
+    //Se não passar nada, 'escapar' é undefined, por isso definir com um valor padrão (false).
+    if (escapar) {
+      this.escapar = escapar;
+    }
   }
 
   public update(model: T): void {
-    const template = this.template(model);
+    let template = this.template(model);
+
+    //Previne tags scripts dentro do template html.
+    if (this.escapar) {
+      template = template.replace(/<script>[\s\S]*?<\/script>/, "");
+    }
     this.elemento.innerHTML = template;
   }
 
--- dist/js/views/view.js
+++ dist/js/views/view.js
@@ -1,9 +1,18 @@
 export class View {
-    constructor(seletor) {
+    constructor(seletor, escapar) {
+        this.escapar = false;
         this.elemento = document.querySelector(seletor);
+        //Se não passar nada, 'escapar' é undefined, por isso definir com um valor padrão (false).
+        if (escapar) {
+            this.escapar = escapar;
+        }
     }
     update(model) {
-        const template = this.template(model);
+        let template = this.template(model);
+        //Previne tags scripts dentro do template html.
+        if (this.escapar) {
+            template = template.replace(/<script>[\s\S]*?<\/script>/, "");
+        }
         this.elemento.innerHTML = template;
     }
 }
